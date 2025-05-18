b6f4622d04c159b3998aec64d0c2b9a4f32119ee
GuillermoUribe03 <guillermouribe.contact@gmail.com>
2025-05-18 03:06:12 -0500

    � Fix: Normalización de finales de línea y control de archivos en el repositorio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
